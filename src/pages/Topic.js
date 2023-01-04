1d9710043558956ee2fee7b6992b83a2a0f73403
Ihsan Alhafiz <rafizmomo@gmail.com>
2023-01-04 23:27:21 +0700

    [Admin Approve] Done Approval

--- src/pages/Topic.js
+++ src/pages/Topic.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import React from "react";
 import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+
 export function TopicPage() {
     const [topics, setTopics] = useState([]);
     // const [topicId, settopicId] = useState("");
